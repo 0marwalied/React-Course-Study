02f49880c3c23eb7f0b92debb513fb9bcaf8871a
omar_walied <zaza03455@gmail.com>
2025-11-12 11:31:14 +0200

    Add initial HTML, CSS, and JavaScript files for JavaScript and TypeScript courses

---
+++ JavaScriptRefresher/notes/notes.js
@@ -0,0 +1,11 @@
+let notes = [
+
+];
+let list = document.querySelector(`ul`);
+
+for (let note of notes) {
+  let li = document.createElement(`li`);
+  li.innerHTML = note;
+  list.appendChild(li);
+  list.appendChild(document.createElement(`hr`));
+}
---
+++ JavaScriptRefresher/src/index.ts
@@ -0,0 +1,11 @@
+document.querySelector(`h1`)!.innerHTML = `Omar ahmed`;
+var isLoggedIn = true;
+
+function printUserName() {
+  var userName: string = "Omar";
+  if (isLoggedIn) {
+  }
+  console.log(userName);
+}
+
+printUserName();
