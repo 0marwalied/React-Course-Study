151ce43e8353b34ec858e5199147b3b5c6d1d8da
omar_walied <zaza03455@gmail.com>
2025-11-22 09:41:34 +0200

    Update folders name

--- TypeScriptCourse/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare enum Directions {
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3
-}
-//# sourceMappingURL=index.d.ts.map
--- TypeScriptCourse/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Directions;
-(function (Directions) {
-    Directions[Directions["Up"] = 0] = "Up";
-    Directions[Directions["Down"] = 1] = "Down";
-    Directions[Directions["Left"] = 2] = "Left";
-    Directions[Directions["Right"] = 3] = "Right";
-})(Directions || (Directions = {}));
-console.log(Directions);
