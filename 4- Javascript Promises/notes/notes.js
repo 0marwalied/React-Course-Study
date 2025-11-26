bd4d2de64261e6b1b03e394bd4aef54d2b95f5bc
omar_walied <zaza03455@gmail.com>
2025-11-26 13:08:52 +0200

    Enhance JavaScript Promises notes and update TypeScript configuration for improved compatibility

--- 4- Javascript Promises/notes/notes.js
+++ 4- Javascript Promises/notes/notes.js
@@ -5,6 +5,13 @@ let notes = [
   `The then() method is used to specify what should happen when a Promise is resolved successfully. It takes two optional arguments: a callback function for the resolved case and another for the rejected case.`,
   `The catch() method is used to handle errors or rejections in a Promise chain. It is called when a Promise is rejected and allows you to define how to respond to the error.`,
   `The finally() method is used to execute code after a Promise has been settled, regardless of whether it was resolved or rejected. It is often used for cleanup actions.`,
+  `<code>Promise.race()</code> is a method that takes an array of Promises and returns a new Promise that settles as soon as one of the input Promises settles (either resolved or rejected).`,
+  `<code>Promise.all()</code> is a method that takes an array of Promises and returns a new Promise that resolves when all of the input Promises have resolved, or rejects if any of them reject.`,
+  `<code>Promise.any()</code> is a method that takes an array of Promises and returns a new Promise that resolves as soon as any one of the input Promises resolves, or rejects if all of them reject.`,
+  `We can write Promise with setTimeout with this two styles
+  <br><br>
+  <code>let promise1 = new Promise<string>((resolve, reject) => setTimeout(resolve, 500, "Promis1"));</code> <br><br>
+  <code>let promise2 = new Promise<string>((resolve, reject) => setTimeout(() => { resolve("Promis2"); }, 200));</code>`,
 ];
 let list = document.querySelector(`ul`);
 
--- 4- Javascript Promises/src/index.ts
+++ 4- Javascript Promises/src/index.ts
@@ -1,26 +1,66 @@
-console.log("Start....");
+type todoTask = {
+  id: number;
+  task: string;
+  userId: number;
+  completed: boolean;
+};
 
-let promise = new Promise((resolve, reject) => {
-  console.log("Promise is pending...");
-  let ok: boolean = true;
-  setTimeout(() => {
-    ok = true;
-    resolve("Promise is resolved successfully!");
-  }, 2000);
-  if (!ok) reject(new Error("Immediate rejection!"));
-});
-console.log("end....");
+type user = {
+  userId: number;
+} | null;
 
-promise
-  .then((value) => {
-    console.log("In then: ", value);
-    return value;
-  })
-  .catch((error) => {
-    console.log("In catch: ", error.message);
-  })
-  .finally(() => {
-    console.log(
-      "In finally: Promise is settled (either resolved or rejected)."
-    );
+function getUserTodos(user: user): Promise<todoTask[]> {
+  let todos: todoTask[] = [];
+  console.log("Fetching todos...");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      todos = [
+        { id: 1, task: "Learn TypeScript", userId: 1, completed: false },
+        { id: 2, task: "Build a project", userId: 1, completed: true },
+        { id: 3, task: "Read a book", userId: 2, completed: false },
+      ];
+      let userTodos: todoTask[] = todos.filter(
+        (todo) => user?.userId && todo.userId === user.userId
+      );
+      if (userTodos.length !== 0) resolve(userTodos);
+      reject("There is no tasks for this user");
+    }, 1500);
   });
+}
+
+function getConstantUser(): Promise<user> {
+  console.log("Fetching user...");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const constantUser: user = { userId: 3 };
+      if (constantUser) resolve(constantUser);
+      reject("There is no user currently");
+    }, 1000);
+  });
+}
+
+// getConstantUser()
+//   .then((user) => {
+//     console.log("Fetched User");
+//     console.log("User: ", user, "\n");
+//     return getUserTodos(user);
+//   })
+//   .then((todos) => {
+//     console.log("User Todos:", todos);
+//     console.log("Todos fetched\n");
+//   })
+//   .catch((error) => console.log(error));
+async function getUserData() {
+  try {
+    const user = await getConstantUser();
+    console.log(user);
+
+    const Todos = await getUserTodos(user);
+    console.log({ Todos });
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUserData();
