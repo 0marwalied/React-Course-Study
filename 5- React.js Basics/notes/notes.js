9d0636955fca16ab346270d9054872a5884b48b0
omar_walied <zaza03455@gmail.com>
2025-11-27 11:19:21 +0200

    Delete files for install vite project

--- 5- React.js Basics/notes/notes.js
+++
@@ -1,9 +0,0 @@
-let notes = [];
-let list = document.querySelector(`ul`);
-
-for (let note of notes) {
-  let li = document.createElement(`li`);
-  li.innerHTML = note;
-  list.appendChild(li);
-  list.appendChild(document.createElement(`hr`));
-}
