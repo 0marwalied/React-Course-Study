f7570b91e0b2df43a389193356f2968af3d2791c
omar_walied <zaza03455@gmail.com>
2026-05-30 15:21:01 +0300

    feat: update RootLayout to use NavLink for navigation and improve user experience with conditional rendering

--- 10-todo-app/src/pages/layouts/index.tsx
+++ 10-todo-app/src/pages/layouts/index.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import Nav from "../../components/ui/Nav";
 import { getLoggedInUserData } from "../../utils/auth";
 import Modal from "../../components/ui/Modal";
@@ -10,25 +10,48 @@ const RootLayout = () => {
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   return (
     <div className="flex flex-col min-h-screen items-center space-y-8">
-      <Nav className="flex bg-indigo-500 text-white p-4 rounded-2xl w-auto min-w-xl justify-between mt-10 font-semibold">
-        <Link to="/">Home</Link>
-        <div className="flex space-x-3">
-          <Link to={loggedInUser ? "/profile" : "/register"}>
-            {loggedInUser ? `Profile` : `Register`}
-          </Link>
-          {loggedInUser && <Link to="/todos">Todos</Link>}
-          <Link
-            to="#"
-            onClick={() => {
-              if (loggedInUser) {
-                setIsLogoutModalOpen(true);
-              }
-            }}
-          >
-            {loggedInUser ? `Logout` : `Login`}
-          </Link>
-        </div>
-      </Nav>
+      {!loggedInUser ? (
+        <Nav
+          className={`flex bg-indigo-500 text-white p-4 rounded-2xl w-auto min-w-xl items-center justify-between mt-10 font-semibold`}
+        >
+          <Link to="/">Home</Link>
+          <div className="flex space-x-3 items-center">
+            <Link to={"/register"}>Register</Link>
+            <Link
+              to="/login"
+              onClick={() => {
+                if (loggedInUser) {
+                  setIsLogoutModalOpen(true);
+                }
+              }}
+              className={`bg-indigo-500  p-2 rounded-md text-white`}
+            >
+              Login
+            </Link>
+          </div>
+        </Nav>
+      ) : (
+        <Nav
+          className={`flex bg-transparent text-black p-4 rounded-2xl w-auto min-w-xl items-center justify-between mt-10 font-semibold`}
+        >
+          <NavLink to="/">Home</NavLink>
+          <div className="flex space-x-3 items-center">
+            <NavLink to={"/profile"}>Profile</NavLink>
+            <NavLink to={"/todos"}>Todos</NavLink>
+            <Link
+              to="#"
+              onClick={() => {
+                if (loggedInUser) {
+                  setIsLogoutModalOpen(true);
+                }
+              }}
+              className={`bg-indigo-500  p-2 rounded-md text-white`}
+            >
+              Logout
+            </Link>
+          </div>
+        </Nav>
+      )}
 
       {loggedInUser && (
         <Modal
