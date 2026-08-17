2f80ddef395eb0b546096d0698303405f5dc98ea
omar_walied <zaza03455@gmail.com>
2026-08-17 14:05:44 +0300

    Use shakra UI

---
+++ 15-full-stack-project/frontend/src/components/ProductCard.tsx
@@ -0,0 +1,38 @@
+import { Button, Card, Image, Text } from "@chakra-ui/react";
+
+const ProductCard = () => {
+  return (
+    <Card.Root maxW="sm" overflow="hidden">
+      <Image
+        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        alt="Green double couch with wooden legs"
+        boxSize="200px"
+        rounded="50%"
+        mx="auto"
+      />
+      <Card.Body gap="2">
+        <Card.Title textAlign={"center"}>Living room Sofa</Card.Title>
+        <Card.Description fontSize="sm" textAlign="center">
+          This sofa is perfect for modern tropical spaces, baroque inspired
+          spaces.
+        </Card.Description>
+        <Text
+          textStyle="2xl"
+          fontWeight="medium"
+          letterSpacing="tight"
+          mt="2"
+          color="purple.600"
+          mx="auto"
+        >
+          $450
+        </Text>
+      </Card.Body>
+      <Card.Footer gap="2">
+        <Button variant="solid">Buy now</Button>
+        <Button variant="ghost">Add to cart</Button>
+      </Card.Footer>
+    </Card.Root>
+  );
+};
+
+export default ProductCard;
