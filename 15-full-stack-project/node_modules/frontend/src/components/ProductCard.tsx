f726cf5b0bba3ee3b6c0135aa9843be8db0be826
omar_walied <zaza03455@gmail.com>
2026-08-19 12:28:02 +0300

    installed axios to call our backend

--- 15-full-stack-project/node_modules/frontend/src/components/ProductCard.tsx
+++ 15-full-stack-project/node_modules/frontend/src/components/ProductCard.tsx
@@ -2,12 +2,12 @@ import { Button, Card, Image, Text } from "@chakra-ui/react";
 import { useColorMode } from "./ui/color-mode";
 import { Link } from "react-router";
 
-const ProductCard = () => {
+const ProductCard = ({ attributes }) => {
   const { colorMode } = useColorMode();
   console.log(colorMode);
 
   return (
-    <Card.Root maxW="sm" border={"1px solid #a8b5c8"} bg="none">
+    <Card.Root border={"1px solid #a8b5c8"} bg="none">
       <Image
         src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         alt="Green double couch with wooden legs"
--- 15-full-stack-project/node_modules/frontend/src/pages/ProductsPage.tsx
+++ 15-full-stack-project/node_modules/frontend/src/pages/ProductsPage.tsx
@@ -1,7 +1,29 @@
 import ProductCard from "@/components/ProductCard";
+import { Grid } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductsPage = () => {
-  return <ProductCard />;
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/api/products")
+      .then((res) => setProductList(res.data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Grid
+      margin="30px"
+      templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      gap="6"
+    >
+      {productList.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </Grid>
+  );
 };
 
 export default ProductsPage;
